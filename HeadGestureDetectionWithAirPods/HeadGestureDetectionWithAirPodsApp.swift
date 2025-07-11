0c5a9d3c346b98222a1947874223098eadfe6969
Kanta Oikawa <iam@kantacky.com>
2025-07-11 18:38:32 +0900

    Add a project

---
+++ HeadGestureDetectionWithAirPods/HeadGestureDetectionWithAirPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadGestureDetectionWithAirPodsApp.swift
+//  HeadGestureDetectionWithAirPods
+//
+//  Created by Kanta Oikawa on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct HeadGestureDetectionWithAirPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeadGestureDetectionWithAirPodsTests/HeadGestureDetectionWithAirPodsTests.swift
@@ -0,0 +1,16 @@
+//
+//  HeadGestureDetectionWithAirPodsTests.swift
+//  HeadGestureDetectionWithAirPodsTests
+//
+//  Created by Kanta Oikawa on 2025/07/11.
+//
+
+import Testing
+
+struct HeadGestureDetectionWithAirPodsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
