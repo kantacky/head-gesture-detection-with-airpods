6957a9629d471bf36b6420a425cda57372b0839a
Kanta Oikawa <iam@kantacky.com>
2025-08-19 13:42:42 +0900

    Update HeadGestureChartView

--- HeadGestureDetectionWithAirPods/HeadGestureChartView.swift
+++ HeadGestureDetectionWithAirPods/HeadGestureChartView.swift
@@ -15,22 +15,8 @@ struct HeadGestureChartView: View {
     var body: some View {
         Chart(motionLogs, id: \.timestamp) { motion in
             let date = Date(timeIntervalSince1970: motion.timestamp)
-            LineMark(
-                x: .value("Date", date),
-                y: .value("UserAcceleration X", motion.userAcceleration.x)
-            )
-            .foregroundStyle(by: .value("UserAcceleration X", "UserAcceleration X"))
-            LineMark(
-                x: .value("Date", date),
-                y: .value("UserAcceleration Y", motion.userAcceleration.y)
-            )
-            .foregroundStyle(by: .value("UserAcceleration Y", "UserAcceleration Y"))
-            LineMark(
-                x: .value("Date", date),
-                y: .value("UserAcceleration Z", motion.userAcceleration.z)
-            )
-            .foregroundStyle(by: .value("UserAcceleration Z", "UserAcceleration Z"))
 
+            // Attitude
             LineMark(
                 x: .value("Date", date),
                 y: .value("Attitude Roll", motion.attitude.roll)
@@ -46,15 +32,85 @@ struct HeadGestureChartView: View {
                 y: .value("Attitude Yaw", motion.attitude.yaw)
             )
             .foregroundStyle(by: .value("Attitude Yaw", "Attitude Yaw"))
+
+            // Gravity
+            LineMark(
+                x: .value("Date", date),
+                y: .value("Gravity X", motion.gravity.x)
+            )
+            .foregroundStyle(by: .value("Gravity X", "Gravity X"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("Gravity Y", motion.gravity.y)
+            )
+            .foregroundStyle(by: .value("Gravity Y", "Gravity Y"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("Gravity Z", motion.gravity.z)
+            )
+            .foregroundStyle(by: .value("Gravity Z", "Gravity Z"))
+
+            // Quaternion
+            LineMark(
+                x: .value("Date", date),
+                y: .value("Quaternion X", motion.attitude.quaternion.x)
+            )
+            .foregroundStyle(by: .value("Quaternion X", "Quaternion X"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("Quaternion Y", motion.attitude.quaternion.y)
+            )
+            .foregroundStyle(by: .value("Quaternion Y", "Quaternion Y"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("Quaternion Z", motion.attitude.quaternion.z)
+            )
+            .foregroundStyle(by: .value("Quaternion Z", "Quaternion Z"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("Quaternion W", motion.attitude.quaternion.w)
+            )
+            .foregroundStyle(by: .value("Quaternion W", "Quaternion W"))
+
+            // RotationRate
+            LineMark(
+                x: .value("Date", date),
+                y: .value("RotationRate X", motion.rotationRate.x)
+            )
+            .foregroundStyle(by: .value("RotationRate X", "RotationRate X"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("RotationRate Y", motion.rotationRate.y)
+            )
+            .foregroundStyle(by: .value("RotationRate Y", "RotationRate Y"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("RotationRate Z", motion.rotationRate.z)
+            )
+            .foregroundStyle(by: .value("RotationRate Z", "RotationRate Z"))
+
+            // UserAcceleration
+            LineMark(
+                x: .value("Date", date),
+                y: .value("UserAcceleration X", motion.userAcceleration.x)
+            )
+            .foregroundStyle(by: .value("UserAcceleration X", "UserAcceleration X"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("UserAcceleration Y", motion.userAcceleration.y)
+            )
+            .foregroundStyle(by: .value("UserAcceleration Y", "UserAcceleration Y"))
+            LineMark(
+                x: .value("Date", date),
+                y: .value("UserAcceleration Z", motion.userAcceleration.z)
+            )
+            .foregroundStyle(by: .value("UserAcceleration Z", "UserAcceleration Z"))
         }
         .chartForegroundStyleScale(
             [
-                "UserAcceleration X": .red,
-                "UserAcceleration Y": .green,
-                "UserAcceleration Z": .blue,
-                "Attitude Roll": .orange,
-                "Attitude Pitch": .mint,
-                "Attitude Yaw": .purple
+                "Attitude Roll": .red,
+                "Attitude Pitch": .green,
+                "Attitude Yaw": .orange,
             ]
         )
         .chartYScale(domain: [-1, 1])
