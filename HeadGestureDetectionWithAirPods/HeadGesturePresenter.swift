5932e68f1a1a87322b0687668c616bf41f32f3c2
Kanta Oikawa <iam@kantacky.com>
2025-08-19 11:33:26 +0900

    Refactor HeadGestureView

--- HeadGestureDetectionWithAirPods/HeadGesturePresenter.swift
+++ HeadGestureDetectionWithAirPods/HeadGesturePresenter.swift
@@ -7,11 +7,15 @@
 
 import CoreMotion
 import Observation
+import RealityKit
 
 @MainActor
 @Observable
 final class HeadGesturePresenter {
+    @MainActor
     struct State {
+        var cubeLeft = ModelEntity()
+        var cubeRight = ModelEntity()
         var motion: CMDeviceMotion?
         var startingPose: CMAttitude?
         var currentPose: CMAttitude?
@@ -21,6 +25,8 @@ final class HeadGesturePresenter {
         case onAppear
         case onDisappear
         case onResetStartingPoseButton
+        case makeRealityView
+        case updateRealityView
     }
 
     private(set) var state = State()
@@ -34,30 +40,104 @@ final class HeadGesturePresenter {
     }
 
     func dispatch(_ action: Action) {
+        Task {
+            await dispatch(action)
+        }
+    }
+
+    func dispatch(_ action: Action) async {
         switch action {
         case .onAppear:
-            trackingTask = Task {
-                do {
-                    for await motion in try headphoneMotionManager.startTracking() {
-                        state.motion = motion
-                        if let startingPose = state.startingPose {
-                            motion.attitude.multiply(byInverseOf: startingPose)
-                        } else {
-                            state.startingPose = motion.attitude
-                        }
-                        state.currentPose = motion.attitude
-                    }
-                } catch {
-                    print("Error starting tracking: \(error)")
-                }
-            }
+            onAppear()
 
         case .onDisappear:
-            trackingTask?.cancel()
-            headphoneMotionManager.stopTracking()
+            onDisappear()
 
         case .onResetStartingPoseButton:
             state.startingPose = nil
+
+        case .makeRealityView:
+            makeRealityView()
+
+        case .updateRealityView:
+            updateRealityView()
+        }
+    }
+}
+
+private extension HeadGesturePresenter {
+    func onAppear() {
+        trackingTask = Task {
+            do {
+                for await motion in try headphoneMotionManager.startTracking() {
+                    state.motion = motion
+                    if let startingPose = state.startingPose {
+                        motion.attitude.multiply(byInverseOf: startingPose)
+                    } else {
+                        state.startingPose = motion.attitude
+                    }
+                    state.currentPose = motion.attitude
+                    updateRealityView()
+                }
+            } catch {
+                print("Error starting tracking: \(error)")
+            }
+        }
+    }
+
+    func onDisappear() {
+        trackingTask?.cancel()
+        headphoneMotionManager.stopTracking()
+    }
+
+    func makeRealityView() {
+        let mesh = MeshResource.generateBox(size: 1, cornerRadius: 0.05)
+        let material = SimpleMaterial(color: .blue, roughness: 0.15, isMetallic: true)
+        state.cubeLeft = ModelEntity(mesh: mesh, materials: [material])
+        state.cubeRight = ModelEntity(mesh: mesh, materials: [material])
+        state.cubeLeft.position = SIMD3(x: -1, y: 0, z: 0)
+        state.cubeRight.position = SIMD3(x: 1, y: 0, z: 0)
+    }
+
+    func updateRealityView() {
+        guard
+            let motion = state.motion,
+            let pose = state.currentPose
+        else {
+            return
+        }
+        switch motion.sensorLocation {
+        case .default:
+            state.cubeLeft.transform.rotation = .init(
+                ix: Float(pose.quaternion.x),
+                iy: Float(pose.quaternion.z),
+                iz: Float(pose.quaternion.y),
+                r: -Float(pose.quaternion.w)
+            )
+            state.cubeRight.transform.rotation = .init(
+                ix: Float(pose.quaternion.x),
+                iy: Float(pose.quaternion.z),
+                iz: Float(pose.quaternion.y),
+                r: -Float(pose.quaternion.w)
+            )
+        case .headphoneLeft:
+            state.cubeLeft.transform.rotation = .init(
+                ix: Float(pose.quaternion.x),
+                iy: Float(pose.quaternion.z),
+                iz: Float(pose.quaternion.y),
+                r: -Float(pose.quaternion.w)
+            )
+            state.cubeRight.transform.rotation = .init(ix: 0, iy: 0, iz: 0, r: 0)
+        case .headphoneRight:
+            state.cubeLeft.transform.rotation = .init(ix: 0, iy: 0, iz: 0, r: 0)
+            state.cubeRight.transform.rotation = .init(
+                ix: Float(pose.quaternion.x),
+                iy: Float(pose.quaternion.z),
+                iz: Float(pose.quaternion.y),
+                r: -Float(pose.quaternion.w)
+            )
+        @unknown default:
+            return
         }
     }
 }
